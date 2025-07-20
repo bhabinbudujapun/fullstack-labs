6be62959db97a6dd3072c72173f4f6c468283c07
Bhabin <poon.bhabin6332@gmail.com>
2025-07-20 21:28:55 +0545

    refactor: listen on PORT, use auth route

--- chatstream/backend/server.js
+++ chatstream/backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoute from "./src/routers/auth.route.js";
 
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Server is running!!");
-});
+const PORT = process.env.PORT || 8000;
 
-app.listen(3000, () => {
-  console.log("Server listen on PORT: 3000");
+app.use(express.json());
+
+app.use("/auth", authRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
