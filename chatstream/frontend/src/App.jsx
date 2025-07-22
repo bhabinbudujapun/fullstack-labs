f2609abe90da115b16278334143c4aa46e44f801
Bhabin <poon.bhabin6332@gmail.com>
2025-07-22 21:06:03 +0545

    initial commit frontend

---
+++ chatstream/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar/Navbar.jsx";
+import Home from "./home/Home.jsx";
+
+function App() {
+  return (
+    <>
+    <Home />
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
