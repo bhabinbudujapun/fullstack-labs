bedaf2f90342c9fa08f22734071859c6c420fd99
Bhabin <poon.bhabin6332@gmail.com>
2025-07-29 22:01:48 +0545

    add: protected route

---
+++ chatstream/frontend/src/components/proute/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+import useTokenStore from "../../store/useAuthStore";
+
+const ProtectedRoute = ({ children }) => {
+  // const token = useTokenStore((state) => state.token);
+  const token = true;
+  return token ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
